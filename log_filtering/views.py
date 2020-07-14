eb61909191c8ceec41fe4b560c03003e63e2d177
Madhavi <madhubs08@gmail.com>
2020-07-14 11:47:04 +0200

    EventLog Modification

--- log_filtering/views.py
+++ log_filtering/views.py
@@ -13,6 +13,9 @@ from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 from pm4py.algo.discovery.dfg import factory as dfg_factory
 import json
 import re
+import log_filtering.abstraction_support_functions as asf
+import log_filtering.utils as utils
+import log_filtering.transformation as trans
 
 
 
@@ -20,29 +23,21 @@ import re
 # Create your views here.
 
 def filter(request):
-    print(request.method)
+    event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
+    event_log = os.path.join(event_logs_path, settings.EVENT_LOG_NAME)
+    log = importer.apply(event_log)
+
+    dfg = dfg_factory.apply(log)
+    this_data,temp_file = dfg_to_g6(dfg)
+    temp_path = os.path.join(settings.MEDIA_ROOT, "temp")
+
     if request.method == 'POST':
         if "uploadButton" in request.POST:
             print("in request")
         event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
-        temp_path = os.path.join(settings.MEDIA_ROOT, "temp")
 
         if settings.EVENT_LOG_NAME == ':notset:':
             return HttpResponseRedirect(request.path_info)
-
-        event_log = os.path.join(event_logs_path, settings.EVENT_LOG_NAME)
-
-        event_log = os.path.join(event_logs_path, settings.EVENT_LOG_NAME)
-        exportPrivacyAwareLog = True
-        log = importer.apply(event_log)
-        dfg = dfg_discovery.apply(log)
-        print("dfg=" + dfg)
-        dfg = dfg_factory.apply(log)
-        print(dfg)
-        this_data = dfg_to_g6(dfg)
-
-
-        network = {}
 
         return render(request,'filter.html', {'log_name': settings.EVENT_LOG_NAME, 'data':this_data})
 
@@ -50,10 +45,63 @@ def filter(request):
         if "groupButton" in request.GET:
             print("in request")
             groupname = request.GET["new_name"]
-            activities = request.GET["groupButton"]
+            pattern = request.GET["values"]
 
-            print(groupname)
-            print(activities)
+            temp_file = os.path.join(temp_path, 'data.json')
+            eventlist = [x for x in pattern.split(',') if x]
+            abs_sequence = {}
+
+            pattern = []
+            with open(temp_file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                for index in eventlist:
+                    # id = int(index)
+                    id = int(index.split("_")[1])
+                    print("id = ", id)
+                    print("event = ", data['nodes'][id]['label'])
+                    pattern.append(data['nodes'][id]['label'])
+
+                # print([])
+                # for nodes in data['nodes']:
+                #     print("id = ", nodes['id'])
+                #     print("label = ", nodes['label'])
+            print(pattern)
+
+
+            pattern_list = [{'ID': 0, 'Name': groupname, 'Pattern': pattern}]
+            print(pattern_list)
+            log = utils.import_log_XES(event_log)
+            concatenated_traces, concatenated_timestamps = asf.read_log(log)
+
+            abstracted_traces, abstracted_timestamps = \
+                asf.perform_abstractions(
+                                [0], pattern_list,
+                                concatenated_traces,
+                                concatenated_timestamps
+                                )
+            print("absracted pattern = ", abstracted_traces)
+
+            log_content = trans.generate_transformed_log_XES(
+                                                event_log,
+                                                abstracted_traces,
+                                                abstracted_timestamps,
+                                                event_log[:-4] + "_header.XES"
+                                                )
+
+            print("log_content = ", log_content)
+
+            # very ugly code to deal with meta data loss of pm4py filters
+            user_abstracted = utils.import_log_XES(event_log[:-4] +
+                                                   "_header.XES")
+
+            print("user_abstracted = ", user_abstracted)
+            dfg = dfg_discovery.apply(user_abstracted)
+            dfg = dfg_factory.apply(log)
+            this_data, temp_file = dfg_to_g6(dfg)
+
+            return render(request,'filter.html', {'log_name': settings.EVENT_LOG_NAME, 'data':this_data})
+
+
         else:
             event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
             temp_path = os.path.join(settings.MEDIA_ROOT, "temp")
@@ -66,7 +114,6 @@ def filter(request):
             event_log = os.path.join(event_logs_path, settings.EVENT_LOG_NAME)
             exportPrivacyAwareLog = True
             log = importer.apply(event_log)
-            dfg = dfg_discovery.apply(log)
             dfg = dfg_factory.apply(log)
             print(dfg)
             this_data,temp_file = dfg_to_g6(dfg)
@@ -99,5 +146,4 @@ def dfg_to_g6(dfg):
     temp_file = os.path.join(temp_path, 'data.json')
     with open(temp_file, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
     return data, temp_file
